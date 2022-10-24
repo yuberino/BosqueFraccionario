3399a3d8b16817feb662550b5ef3633152fb279e
yuberino <xavieryubero@ceroca.cat>
2022-10-24 17:21:46 +0200

    Menu_Inicial

---
+++ Assets/Scripts/FuncionBotonSalir.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuncionBotonSalir : MonoBehaviour
+{
+    public void ExitGame()
+    {
+
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    
+    }
+}
